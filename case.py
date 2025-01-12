87d5d338ed29d630340c2fdd4f226b4ecda1d1ed
FreeZixDG <dogeusdoge@gmail.com>
2025-01-12 21:44:25 +0100

    adddes

--- case.py
+++ case.py
@@ -1,37 +1,41 @@
 import pygame as pg
 
+from team import Team
+
 
 class Case:
-    def __init__(self, coordinates: tuple[int, int], team: bool = None, is_queen: bool = False):
-        self.x, self.y = coordinates
-        self.team = team
-        self.is_queen = is_queen
-        self.is_selected = False
+    def __init__(self, coordinates: tuple[int, int], team: Team = None, is_queen: bool = False):
+        self.__x, self.__y = coordinates
+        self.__team = team
+        self.__is_queen = is_queen
+        self.__is_selected = False
 
-        self.color = "#713a36" if (self.x + self.y) % 2 == 0 else "#ffcb98"
+        self.__color = "#713a36" if (self.__x + self.__y) % 2 == 0 else "#ffcb98"
 
     def draw(self, surface: pg.Surface, size: int, offset: int = 0) -> None:
-        self.draw_square(surface, size, offset)
-        self.draw_piece(surface, size, offset)
+        self.__draw_square(surface, size, offset)
+        self.__draw_piece(surface, size, offset)
 
-    def draw_piece(self, surface: pg.Surface, size: int, offset: int = 0) -> None:
-        if self.team is None:
-            return
+    def __draw_piece(self, surface: pg.Surface, size: int, offset: int = 0) -> None:
 
-        if self.team:
-            c = "#EEEEEE"
-        else:
-            c = "#111111"
+        match self.__team:
+            case Team.WHITE:
+                c = "#EEEEEE"
+            case Team.BLACK:
+                c = "#111111"
+            case _:
+                return
 
-        pg.draw.circle(surface, c, (self.x * (size + offset) + size / 2, self.y * (size + offset) + size / 2), size / 2)
+        pg.draw.circle(surface, c, (self.__x * (size + offset) + size / 2, self.__y * (size + offset) + size / 2),
+                       size / 2)
 
-    def draw_square(self, surface: pg.Surface, size: int, offset: int = 0) -> None:
-        pg.draw.rect(surface, self.color, pg.Rect(self.x * (size + offset), self.y * (size + offset), size, size))
+    def __draw_square(self, surface: pg.Surface, size: int, offset: int = 0) -> None:
+        pg.draw.rect(surface, self.__color, pg.Rect(self.__x * (size + offset), self.__y * (size + offset), size, size))
 
     def __repr__(self) -> str:
-        result = f"{self.__class__.__name__}({self.x}, {self.y})"
-        if self.team is not None:
-            result += f" ({self.team})"
-        if self.is_queen:
+        result = f"{self.__class__.__name__}({self.__x}, {self.__y})"
+        if self.__team is not None:
+            result += f" ({self.__team.value})"
+        if self.__is_queen:
             result += " Queen"
         return result
---
+++ team.py
@@ -0,0 +1,6 @@
+from enum import Enum
+
+
+class Team(Enum):
+    WHITE = "White"
+    BLACK = "Black"
