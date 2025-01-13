e1d1d6ac5ec460145b431de26162ac6ee06e8738
jerem <dogeusdoge@gmail.com>
2025-01-13 15:54:24 +0100

    factorized color

--- piece.py
+++ piece.py
@@ -7,24 +7,24 @@ class Piece:
     def __init__(self, team: Team):
         self.__team = team
 
+        match self.__team:
+            case Team.WHITE:
+                self._color_in = "#DDDDDD"
+                self._color_out = "#333333"
+            case Team.BLACK:
+                self._color_in = "#333333"
+                self._color_out = "#DDDDDD"
+            case _:
+                return
+
     def get_team(self):
         return self.__team
 
     def draw(self, surface: pg.Surface, location: tuple[int, int], size: int, offset: int = 0) -> None:
 
-        match self.__team:
-            case Team.WHITE:
-                color_in = "#DDDDDD"
-                color_out = "#333333"
-            case Team.BLACK:
-                color_in = "#333333"
-                color_out = "#DDDDDD"
-            case _:
-                return
-
-        pg.draw.circle(surface, color_out,
+        pg.draw.circle(surface, self._color_out,
                        (location[0] * (size + offset) + size / 2, location[1] * (size + offset) + size / 2), size / 2)
-        pg.draw.circle(surface, color_in,
+        pg.draw.circle(surface, self._color_in,
                        (location[0] * (size + offset) + size / 2, location[1] * (size + offset) + size / 2), size / 2.2)
 
     def __repr__(self):
@@ -38,16 +38,8 @@ class Queen(Piece):
 
     def draw(self, surface: pg.Surface, location: tuple[int, int], size: int, offset: int = 0) -> None:
         super().draw(surface, location, size, offset)
-        match self.get_team():
-            case Team.WHITE:
-                c = "#333333"
-            case Team.BLACK:
-                c = "#DDDDDD"
-            case _:
-                return
-
-        pg.draw.circle(surface, c, (location[0] * (size + offset) + size / 2, location[1] * (size + offset) + size / 2),
-                       size / 6)
+        pg.draw.circle(surface, self._color_out,
+                       (location[0] * (size + offset) + size / 2, location[1] * (size + offset) + size / 2), size / 6)
 
     def __repr__(self):
         if self.get_team() is not None:
