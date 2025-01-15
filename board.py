ec829b675c169ddb184e85af4f3027cbf9183c69
jerem <dogeusdoge@gmail.com>
2025-01-15 21:53:27 +0100

    Refactor board initialization with compact encoding support
    Enhanced the board initialization to support a compact string-based encoding format for defining initial board states. Introduced helper functions `moddiv` and `put_playable_case` for better modularity and readability. Adjusted `Case` to handle both tuple and integer-based coordinates for flexibility.

--- board.py
+++ board.py
@@ -1,3 +1,5 @@
+import re
+
 import numpy as np
 import pygame as pg
 
@@ -15,30 +17,53 @@ def add(t: tuple[int | float, ...], c: int | float):
     return tuple(map(lambda x: x + c, t))
 
 
+def moddiv(a, b):
+    return a % b, a // b
+
 class Board:
-    def __init__(self, size: int, init: str = None):
+    def __init__(self, size: int, init: str = "20B10.20w"):
         self.__size = size
-        if init is None:
-            self.init = "20b10.20w"
-        else:
-            self.init = init
         self.__board = np.zeros((size, size), dtype=Case)
 
-        for y in range(size):
-            for x in range(size):
-                if (x + y) % 2 == 0:
+        self.init = [(int(num) if num else 1, char) for num, char in re.findall(r"(\d*)([wW]|[bB]|[.])", init)]
+        total = 0
+        for num, char in self.init:
+            for i in range(num):
+                i += total
+                x, y = moddiv(i * 2, size)
+                if (i // (size // 2)) % 2 == 0:
                     self.__board[x, y] = Case((x, y))
-                    continue
-
-                if y >= 6:
-                    self.__board[x, y] = PlayableCase((x, y), content=Piece(Team.WHITE))
-                elif y <= 3:
-                    self.__board[x, y] = PlayableCase((x, y), content=Piece(Team.BLACK))
+                    x, y = moddiv(i * 2 + 1, size)
+                    self.put_playable_case((x, y), char)
                 else:
-                    self.__board[x, y] = PlayableCase((x, y))
+                    self.put_playable_case((x, y), char)
+                    x, y = moddiv(i * 2 + 1, size)
+                    self.__board[(x, y)] = Case((x, y))
+            total += num
 
-                if x == 5 and y == 6:
-                    self.__board[x, y] = PlayableCase((x, y), content=Queen(Team.WHITE))
+    def put_playable_case(self, coordinates: int | tuple[int, int], char: str):
+        match char:
+            case 'b':
+                team = Team.BLACK
+                piece_type = Piece
+            case 'w':
+                team = Team.WHITE
+                piece_type = Piece
+            case 'B':
+                team = Team.BLACK
+                piece_type = Queen
+            case 'W':
+                team = Team.WHITE
+                piece_type = Queen
+            case '.':
+                team = None
+                piece_type = None
+            case _:
+                raise ValueError("Invalid character in init string")
+        if team is not None:
+            self.__board[coordinates] = PlayableCase(coordinates, content=piece_type(team))
+        else:
+            self.__board[coordinates] = PlayableCase(coordinates)
 
     def draw(self, screen: pg.Surface, size: int, offset: int = 0):
         for row in self.__board:
